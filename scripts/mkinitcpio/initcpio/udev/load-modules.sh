3d7ce99bf2c7cbc682814466400b477ec5c54851
Christopher Rogers <slaxemulator@gmail.com>
2010-01-14 15:21:08 -0800

    Updated load-modules.sh script.
    Signed-off-by: Christopher Rogers <slaxemulator@gmail.com>

--- scripts/mkinitcpio/initcpio/udev/load-modules.sh
+++ scripts/mkinitcpio/initcpio/udev/load-modules.sh
@@ -1,14 +1,13 @@
 #! /bin/sh
 # Implement blacklisting for udev-loaded modules
 #   Includes module checking
-# - Aaron Griffin & Tobias Powalowski for Archlinux
+# - Aaron Griffin, Tobias Powalowski & Thomas Bächler for Arch Linux
 [ $# -ne 1 ] && exit 1
 
 MODPROBE="/sbin/modprobe"
-RESOLVEALIAS="/bin/resolve-modalias"
+RESOLVEALIAS="${MODPROBE} --resolve-alias"
 USEBLACKLIST="--use-blacklist"
 SED="/bin/sed"
-MODDEPS="/bin/moddeps"
 
 if [ -f /proc/cmdline ]; then
   for cmd in $(cat /proc/cmdline); do
@@ -28,14 +27,20 @@ BLACKLIST="$(echo "${BLACKLIST}" | ${SED} 's|-|_|g')"
 
 if [ -n "${BLACKLIST}" ] ; then
   # Try to find all modules for the alias
-  mods="$($RESOLVEALIAS /lib/modules/$(uname -r)/modules.alias $1)"
+  mods="$($RESOLVEALIAS $1)"
   # If no modules could be found, try if the alias name is a module name
   # In that case, omit the --use-blacklist parameter to imitate normal modprobe behaviour
   [ -z "${mods}" ] && $MODPROBE -qni $1 && mods="$1" && USEBLACKLIST=""
   [ -z "${mods}" ] && exit
   for mod in ${mods}; do
-    deps="$(${MODDEPS} ${mod})"
+    # Find the module and all its dependencies
+    deps="$($MODPROBE -i --show-depends ${mod})"
     [ $? -ne 0 ] && continue
+
+    #sanitize the module names
+    deps="$(echo "$deps" | ${SED} \
+            -e "s#^insmod /lib.*/\(.*\)\.ko.*#\1#g" \
+            -e 's|-|_|g')"
     # If the module or any of its dependencies is blacklisted, don't load it
     for dep in $deps; do
       for blackmod in ${BLACKLIST}; do
