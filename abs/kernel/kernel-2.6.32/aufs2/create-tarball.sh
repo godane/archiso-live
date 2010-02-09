b59ef552ffcc1f336bdf36653ef6655a933c2048
Christopher Rogers <slaxemulator@gmail.com>
2010-02-09 03:40:05 -0800

    Updated aufs2 kernel module.
    Signed-off-by: Christopher Rogers <slaxemulator@gmail.com>

--- abs/kernel/kernel-2.6.32/aufs2/create-tarball.sh
+++ abs/kernel/kernel-2.6.32/aufs2/create-tarball.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 AUFS2VERSION="-32"
 KERNELVERSION=2.6.32
-GITSNAPSHOT=20100116
+GITSNAPSHOT=20100207
 # aufs2 (no -xx) for the latest -rc version.
-git clone http://git.c3sl.ufpr.br/pub/scm/aufs/aufs2-standalone.git aufs2-standalone.git
+if [ ! -d aufs2-standalone.git/.git ]; then
+	git clone http://git.c3sl.ufpr.br/pub/scm/aufs/aufs2-standalone.git aufs2-standalone.git
+else
+	git pull
+fi
 cd aufs2-standalone.git
 git checkout origin/aufs2${AUFS2VERSION}
 #*** apply "aufs2-base.patch" and "aufs2-standalone.patch" to your kernel source files.
